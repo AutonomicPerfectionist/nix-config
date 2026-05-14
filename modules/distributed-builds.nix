fb1888f3e0114fbe4f2b04dd6032272acc6bc1e8
Branden Butler <bwtbutler@hotmail.com>
2026-05-14 17:10:02 -0500

    feat: Add distributed build config

---
+++ modules/distributed-builds.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  nix.distributedBuilds = true;
+  nix.settings.builders-use-substitutes = true;
+
+  nix.buildMachines = [
+    {
+      hostName = "thunder-budget-3.local";
+      sshUser = "remotebuild";
+      sshKey = "/root/.ssh/remotebuild";
+      system = pkgs.stdenv.hostPlatform.system;
+      maxJobs = 8;
+      supportedFeatures = [ "nixos-test" "big-parallel" "kvm" "gccarch-ivybridge"];
+    }
+
+    {
+      hostName = "thunder-budget-4.local";
+      sshUser = "remotebuild";
+      sshKey = "/root/.ssh/remotebuild";
+      system = pkgs.stdenv.hostPlatform.system;
+      maxJobs = 8;
+      supportedFeatures = [ "nixos-test" "big-parallel" "kvm" "gccarch-ivybridge"];
+    }
+  ];
+}
---
+++ users/remotebuild/default.nix
@@ -0,0 +1,33 @@
+{
+  users.users.remotebuild = {
+    isSystemUser = true;
+    group = "remotebuild";
+    useDefaultShell = true;
+
+    openssh.authorizedKeys.keyFiles = [
+    	./fatman-3.pub 
+    	./big-nix.pub
+  	];
+  };
+
+  users.groups.remotebuild = {};
+
+  nix = {
+      nrBuildUsers = 64;
+      settings = {
+        trusted-users = [ "remotebuild" ];
+  
+        min-free = 10 * 1024 * 1024;
+        max-free = 200 * 1024 * 1024;
+  
+        max-jobs = "auto";
+        cores = 0;
+      };
+    };
+  
+    systemd.services.nix-daemon.serviceConfig = {
+      MemoryAccounting = true;
+      MemoryMax = "90%";
+      OOMScoreAdjust = 500;
+    };
+}

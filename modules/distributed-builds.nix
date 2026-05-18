f4ce32322bf24f968fd0dd2513e6c587d19db811
Branden Butler <bwtbutler@hotmail.com>
2026-05-18 16:28:52 -0500

    fix: Disable thunder-budget-3 as distributed builder due to disk space issue

--- modules/distributed-builds.nix
+++ modules/distributed-builds.nix
@@ -4,14 +4,14 @@
   nix.settings.builders-use-substitutes = true;
 
   nix.buildMachines = [
-    {
-      hostName = "thunder-budget-3.local";
-      sshUser = "remotebuild";
-      sshKey = "/root/.ssh/remotebuild";
-      system = pkgs.stdenv.hostPlatform.system;
-      maxJobs = 8;
-      supportedFeatures = [ "nixos-test" "big-parallel" "kvm" "gccarch-ivybridge"];
-    }
+    # {
+    #   hostName = "thunder-budget-3.local";
+    #   sshUser = "remotebuild";
+    #   sshKey = "/root/.ssh/remotebuild";
+    #   system = pkgs.stdenv.hostPlatform.system;
+    #   maxJobs = 8;
+    #   supportedFeatures = [ "nixos-test" "big-parallel" "kvm" "gccarch-ivybridge"];
+    # }
 
     {
       hostName = "thunder-budget-4.local";

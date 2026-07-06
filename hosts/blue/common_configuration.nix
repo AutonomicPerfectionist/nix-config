c15abac842ce593dae28cead42065b27191bf12c
Branden Butler <bwtbutler@hotmail.com>
2026-07-05 20:59:37 -0500

    feat: Switch king-blue over to titan v

--- hosts/blue/common_configuration.nix
+++ hosts/blue/common_configuration.nix
@@ -15,6 +15,7 @@
     ../../modules/ml/llama.nix
     ../../hardware/gpus/gpus.nix
     ../../modules/common/overlays.nix
+    # ../../containers/vllm.nix
     flake-inputs.home-manager.nixosModules.default
 
     # Users
@@ -32,8 +33,14 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  boot.kernelParams = [
+    "pci=realloc"
+  ];
   hardware.graphics.enable = true;
-  hardware.gpu.intel.enable = true;
+  # hardware.gpu.intel.enable = true;
+  hardware.gpu.nvidia.enable = true;
+  # The Titan V has the exact same chip as the V100
+  hardware.gpu.nvidia.cards = [ "tesla_v100" ];
 
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
@@ -49,6 +56,10 @@
   ];
 
   services.openssh.enable = true;
+  services.llama.rpcServer.enable = true;
+  services.llama.useCustomSource = true;
+  # services.vllm-xpu.enable = true;
+  # services.vllm-xpu.model = "NousResearch/Hermes-3-Llama-3.1-8B";
 
   system.stateVersion = "25.11";
 }
